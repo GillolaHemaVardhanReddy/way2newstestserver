ca5e305a8446182f968f0966dae3653539710a03
GillolaHemaVardhanReddy <gillolahemavardhanreddy@gmail.com>
2025-02-20 18:22:33 +0530

    changed cors

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -34,6 +34,8 @@ export const signin = async (req,res,next)=>{
         res.cookie('auth',token, {
             maxAge: 1000 * 60 * 60 * 24 * 10,
              signed: true,
+             httpOnly:true,
+             sameSite: 'None'
             } ).status(200).json({success:true,data:remain})
     }catch(err){
         console.log(chalk.red.bgWhite('error in signin controller: ',err.message))
